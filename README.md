4774ac641f09725dbc3de0fdfd308e73efc0782d
Jéser Cáceres Marcelino <jeser.caceres01@yahoo.com>
2024-05-14 14:44:06 -0300

    exclusão arquivos duplicados

--- README.md
+++
@@ -1,1 +0,0 @@
-<h1>Welcome to Petspot</h1>
--- src/main/resources/db/migration/V1__create-table-login.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE login (
-   id VARCHAR(36) PRIMARY KEY,
-   email VARCHAR(255) UNIQUE,
-   password_login VARCHAR(255),
-   type_of_user ENUM('PETOWNER')
-);
